9de24d6668277957bac0a00646fb9e7ae44c9763
Chitty Liu <d2921118@gmail.com>
2018-08-14 16:13:33 +0100

    structure done

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # read
   get "tasks", to: "tasks#index"
-  # read one task
-  get "tasks", to: "tasks#show"
   # create
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new
   post "tasks", to: "tasks#create"
+  # read one task
+  get "tasks/:id", to: "tasks#show", as: :task
   # update
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
   # delete
-  get "tasks/:id", to: "tasks#destroy"
+  delete "tasks/:id", to: "tasks#destroy"
 end
